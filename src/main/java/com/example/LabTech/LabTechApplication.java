63099a6872a581749b89c471fe7357cff10da36f
sofiaGit <sofia@gmail.>
2024-01-22 14:30:55 +0100

    add Swagger to project

--- src/main/java/com/example/LabTech/LabTechApplication.java
+++ src/main/java/com/example/LabTech/LabTechApplication.java
@@ -2,7 +2,11 @@ package com.example.LabTech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@EnableWebMvc
 @SpringBootApplication
 public class LabTechApplication {
 
---
+++ src/main/java/com/example/LabTech/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.example.LabTech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.LabTech.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.LabTech.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("LabXpert Spring Boot REST API Documentation")
+                .description("REST APIs For Managing a labo")
+                .version("1.0")
+                .build();
+    }
+}
+
+*/
